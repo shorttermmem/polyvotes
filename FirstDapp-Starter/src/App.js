70ea543d2268d937f3c632f5a6a388b419742aab
stm <no-reply@nothing.com>
2023-01-22 21:56:39 -0500

    SWDEV-1 - Test launch react app with 'yarn start'

--- FirstDapp-Starter/src/App.js
+++ FirstDapp-Starter/src/App.js
@@ -6,7 +6,8 @@ const App = () => {
 
   return (
     <>
-     
+      <div> US Debt Ceiling </div>
+      <a href="https://en.wikipedia.org/wiki/History_of_the_United_States_debt_ceiling">US Debt Ceiling</a>
     </>
   );
 };
